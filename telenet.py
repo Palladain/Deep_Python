571b6c8f76f202974da11d7c650366cfbf0a107d
Palladain <petrov-timur@mail.ru>
2022-10-10 03:00:21 +0300

    Add Lecture_6

--- telenet.py
+++
@@ -1,9 +0,0 @@
-import telnetlib #библиотека для подключений
-
-print("connecting...")
-tn = telnetlib.Telnet("www.python.org", '80')
-msg = "GET / HTTP/1.1\nHost:www.python.org\n\n".encode('ascii')
-tn.write(msg)
-print(tn.read_until(b"\r"))
-print("response received")
-tn.close()
